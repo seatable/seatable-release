34af8c4f85fd4603877831e3ef8a552620056af8
Simon Hammes <simonhammes@users.noreply.github.com>
2024-11-27 18:30:46 +0100

    Add user-stats.sh

---
+++ compose/tools/user-stats.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source /opt/seatable-compose/.env
+
+LICENSE_FILE="/opt/seatable-compose/seatable-license.txt"
+LICENSE_FILE="seatable-license.txt"
+
+USER_LIMIT=$(grep -Po '(?<=MaxUsers = \")(\d+)' ${LICENSE_FILE})
+echo "User limit according to license file: ${USER_LIMIT}"
+
+QUERY="SELECT COUNT(*) AS users, (SELECT COUNT(*) FROM ccnet_db.EmailUser WHERE is_active = 1) AS active_users FROM ccnet_db.EmailUser;"
+
+echo -e "\nUsers in database:"
+docker exec -it mariadb mariadb -uroot -p"${SEATABLE_MYSQL_ROOT_PASSWORD}" -e "${QUERY}"
