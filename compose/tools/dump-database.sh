aacd337ef28afc14410aab70375a9a1312381f25
Simon Hammes <simonhammes@users.noreply.github.com>
2024-11-28 11:25:36 +0100

    Add timestamps to filenames

--- compose/tools/dump-database.sh
+++ compose/tools/dump-database.sh
@@ -6,11 +6,10 @@ BACKUP_DIRECTORY=/opt/seatable-backup
 
 mkdir -p "${BACKUP_DIRECTORY}"
 
-# TODO: Update commands on admin.seatable.io: mysqldump -> mariadb-dump
-# TODO: Add timestamp to filenames?
+TIME=$(date +%Y-%m-%d-%H-%M-%S)
 
-docker exec mariadb bash -c 'mariadb-dump -uroot -p"${MYSQL_ROOT_PASSWORD}" --opt ccnet_db' > "${BACKUP_DIRECTORY}/ccnet_db.sql"
-docker exec mariadb bash -c 'mariadb-dump -uroot -p"${MYSQL_ROOT_PASSWORD}" --opt dtable_db' > "${BACKUP_DIRECTORY}/dtable_db.sql"
-docker exec mariadb bash -c 'mariadb-dump -uroot -p"${MYSQL_ROOT_PASSWORD}" --opt seafile_db' > "${BACKUP_DIRECTORY}/seafile_db.sql"
+docker exec mariadb bash -c 'mariadb-dump -uroot -p"${MYSQL_ROOT_PASSWORD}" --opt ccnet_db' > "${BACKUP_DIRECTORY}/ccnet_db-${TIME}.sql"
+docker exec mariadb bash -c 'mariadb-dump -uroot -p"${MYSQL_ROOT_PASSWORD}" --opt dtable_db' > "${BACKUP_DIRECTORY}/dtable_db-${TIME}.sql"
+docker exec mariadb bash -c 'mariadb-dump -uroot -p"${MYSQL_ROOT_PASSWORD}" --opt seafile_db' > "${BACKUP_DIRECTORY}/seafile_db-${TIME}.sql"
 
 echo "Success: Dumped databases into ${BACKUP_DIRECTORY}"
